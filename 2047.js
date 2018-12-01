3deb2ed8ce93699bd9af51277aa452a7bc8bd89d
Jason Kwok <JasonHK@users.noreply.github.com>
2018-12-02 00:50:00 +0800

    Website optimization * index.html are now easily to read * better RWD stylesheet * ES2015 style JavaScript and bug fixes + added .gitignore

--- 2047.js
+++ 2047.js
@@ -1,32 +1,33 @@
-// Set the date we're counting down to
-var countdownDate = new Date("Jul 1, 2047 00:00:00 GMT+08").getTime();
+"use strict";
 
-// Update the count down every 1 second
-var countdown = setInterval(function() {
+let countdownDate = new Date("Jul 1, 2047 00:00:00 GMT+08").getTime();
 
-    // Get todays date and time
-    var now = new Date().getTime();
+let countdown = setInterval(() => {
+    let now = new Date().getTime();
+    let distance = countdownDate - now;
 
-    // Find the distance between now an the count down date
-    var distance = countdownDate - now;
+    let years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365.25));
+    let days = Math.floor(distance % (1000 * 60 * 60 * 24 * 365.25) / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Time calculations for years, days, hours, minutes and seconds
-    var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365.25));
-    var days = Math.floor(distance % (1000 * 60 * 60 * 24 * 365.25) / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element
     document.getElementById("years").innerHTML = years;
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 
-    // If the count down is finished, write some text 
     if (distance < 0) {
         clearInterval(countdown);
+
+        document.getElementById("years").innerHTML = "0";
+        document.getElementById("days").innerHTML = "0";
+        document.getElementById("hours").innerHTML = "0";
+        document.getElementById("minutes").innerHTML = "0";
+        document.getElementById("seconds").innerHTML = "0";
+    
+        document.getElementById("done").hidden = false;
         document.getElementById("done").classList.remove('disabled');
         document.getElementById("done").classList.add('active');
         document.getElementById("countdown").classList.add('disabled');
